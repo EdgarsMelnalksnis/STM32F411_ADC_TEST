5a4b6dd0b30f60e4f6aa1399648211d0835a35a7
Krisjans Blukis <krisjans@mikrotik.com>
2019-02-04 12:46:21 +0200

    Use proper ADC1 structure variable instead of manually calculating address.

--- STM32F411_ADC_TEST/src/main.c
+++ STM32F411_ADC_TEST/src/main.c
@@ -209,7 +209,7 @@ void init_ADC1(void) {
 
 void init_DMA2(void) {
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
-    DMA2_Stream0->PAR = (uint32_t)(ADC1_BASE + 0x4c); //set ADC1_DR port address DMA_SxPAR
+    DMA2_Stream0->PAR = (uint32_t)(&ADC1->DR);
     DMA2_Stream0->M0AR = (uint32_t) &adc1DmaWMem[0]; // SRAM adrese..vajag aspkatiities, vai stack kkur tur neparaskta
     DMA2_Stream0->NDTR = ITERATIONS; //number of data to be transferred
     DMA2_Stream0->CR |= (DMA_SxCR_CIRC | DMA_SxCR_MINC | DMA_SxCR_PSIZE_0
