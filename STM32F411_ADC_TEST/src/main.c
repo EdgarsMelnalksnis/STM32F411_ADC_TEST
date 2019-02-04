36f756940efb6402d4e667f596a43ef4e83525c0
Krisjans Blukis <krisjans@mikrotik.com>
2019-02-04 12:08:02 +0200

    Use proper DMA2 structure variable instead of manuly calculating address.

--- STM32F411_ADC_TEST/src/main.c
+++ STM32F411_ADC_TEST/src/main.c
@@ -210,7 +210,7 @@ void init_ADC1(void) {
 void init_DMA2(void) {
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
     DMA2_Stream0->PAR = (uint32_t)(ADC1_BASE + 0x4c); //set ADC1_DR port address DMA_SxPAR
-    (*((uint32_t *) (DMA2_Stream0_BASE + 0xcu))) = (uint32_t) &adc1DmaWMem[0]; // SRAM adrese..vajag aspkatiities, vai stack kkur tur neparaskta
+    DMA2_Stream0->M0AR = (uint32_t) &adc1DmaWMem[0]; // SRAM adrese..vajag aspkatiities, vai stack kkur tur neparaskta
     (*((uint32_t *) (DMA2_Stream0_BASE + 0x4u))) = ITERATIONS; //number of data to be transferred
     (*((uint32_t *) (DMA2_Stream0_BASE + 0x0u))) |= (1 << 8 | 1 << 10 | 1 << 11
 	    | 1 << 13); //(1<<8|1<<10|1<<12|1<<14);//circular mode,memory pointer is incremented,32 bit peripheral and 32 bit memory
